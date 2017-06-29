ec15479cc5ff43fcaacdb1bffc4acf313476f478
Imani Greenidge <igreenidge2018@bklawtech.com>
2017-06-29 20:19:00 +0000

    fine

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
-   
+     function name() {
+       var userName = $("#name").val();
+    };
     
+    
+     function email (){
+      var email =  $("#email").val();
+    };
+    
+    $("#signUp").click(function() {
+
+        // console.log("Hi");
+           alert("Hi" + userName + " " + " " + "thank you for signing up! We will send you an email to" + " " + email + " " + " shortly");
+    });
+
+
 
 });
